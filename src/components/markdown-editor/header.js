cffa0aa0e0e85921aa9fd50c4b676d4e10b22424
gilvan <g.r.almeida@live.com>
2019-02-03 17:54:08 -0200

    Creating MD Files

--- src/components/markdown-editor/header.js
+++ src/components/markdown-editor/header.js
@@ -2,16 +2,18 @@
 
 import React, { PropTypes } from 'react'
 
-const MarkdownEditorHeader = ({ isSaving, handleRemove }) => (
+const MarkdownEditorHeader = ({ isSaving, handleRemove, handleCreate }) => (
   <header className='editor-header'>
     <p className='save-message'>{isSaving ? 'Salvando...' : 'Salvo!'}</p>
+    <button onClick={handleCreate}>Criar novo</button>
     <button onClick={handleRemove} className='button-remove'>Remover</button>
   </header>
 )
 
 MarkdownEditorHeader.propTypes = {
   isSaving: PropTypes.bool.isRequired,
-  handleRemove: PropTypes.func.isRequired
+  handleRemove: PropTypes.func.isRequired,
+  handleCreate: PropTypes.func.isRequired
 }
 
 export default MarkdownEditorHeader
--- src/components/markdown-editor/index.js
+++ src/components/markdown-editor/index.js
@@ -1,24 +1,22 @@
 'use strict'
 
 import React, { PropTypes } from 'react'
-import Header from './markdown-editor-header'
+import Header from './header'
 
 const MarkdownEditor = ({
   value,
-  isSaving,
   handleChange,
-  handleRemove,
-  getMarkup
+  getMarkup,
+  textareaRef,
+  ...props
 }) => (
   <section className='editor'>
-    <Header
-      isSaving={isSaving}
-      handleRemove={handleRemove}
-    />
+    <Header {...props} />
     <textarea
       value={value}
       onChange={handleChange}
       autoFocus
+      ref={textareaRef}
     />
     <article
       className='view'
@@ -30,7 +28,8 @@ const MarkdownEditor = ({
 MarkdownEditor.propTypes = {
   value: PropTypes.string.isRequired,
   handleChange: PropTypes.func.isRequired,
-  getMarkup: PropTypes.func.isRequired
+  getMarkup: PropTypes.func.isRequired,
+  textareaRef: PropTypes.func.isRequired
 }
 
 export default MarkdownEditor
